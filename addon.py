a5f0ec488a7b5c6471a3f19d444c2b1b22d0a0c7
computer-geek64 <sudouser512@gmail.com>
2020-04-01 08:40:28 -0400

    Update

--- addon.py
+++ addon.py
@@ -1,7 +1,7 @@
 import sys
-import urllib
+#import urllib
 import xbmcgui
-import urlparse
+#import urlparse
 import xbmcaddon
 
 
@@ -11,12 +11,12 @@ addon_name = addon.getAddonInfo('name')
 # Get init data
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
-args = urlparse.parse_qs(sys.argv[2][1:])
+#args = urlparse.parse_qs(sys.argv[2][1:])
 xbmcplugin.setContent(addon_handle, 'movies')
-mode = ars.get('action', None)
+#mode = args.get('action', None)
 
 # Functions
-def build_url(query):
+"""def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
 
 # Set a string variable to use
@@ -30,9 +30,10 @@ line1 = 'This is a Kodi add-on'
 if mode is None:
     listing = [
         (build_url({'action': 'listing', 'folder': 'movies'}), xbmcgui.ListItem('Movies', iconimage='DefaultFolder.png'), True),
-        (build_url({'action': 'listing', 'folder': 'endpoints'}), xbmcgui.ListItem('Endpoints'), iconImage='DefaultFolder.png'), True)
+        (build_url({'action': 'listing', 'folder': 'endpoints'}), xbmcgui.ListItem('Endpoints', iconImage='DefaultFolder.png'), True)
     ]
     xbmcplugin.addDirectoryItems(addon_handle, listing, len(listing))
     xbmcplugin.endOfDirectory(addon_handle)
 elif mode[0] == 'pass':
     xbmcgui.Dialog().ok(addon_name, 'Welcome to ' + args['folder'][0] + '!')
+"""
